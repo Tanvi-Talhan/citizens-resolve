2b02e0cf321094e5ff4555cb45b079b97fae4072
SanjanaMankar <sanjanamankar22@gnmail.com>
2024-08-02 10:59:14 +0530

    app page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReportIssue from './pages/ReportIssue';
-// import Home from './pages/Home';
 import MainLayout from './components/MainLayout';
-import Home from './pages/Home.jsx';
-// import ReportIssue from './pages/ReportIssue.jsx';
 import IssueTracker from './pages/IssueTracker.jsx';
 import GovernmentAction from './pages/GovernmentAction.jsx';
 import NewsAndUpdate from './pages/NewsAndUpdate.jsx';
@@ -17,21 +14,18 @@ import GuidelinesAndSafety from './pages/GuidelinesAndSafety.jsx';
 
 function App() {
   return (
-    // <ReportIssue />
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          {/* <Route index element={<Home />} /> */}
           <Route path="report-issue" element={<ReportIssue />} />
           <Route path="issue-tracker" element={<IssueTracker />} />
           <Route path="government-action" element={<GovernmentAction />} />
           <Route path="news-and-update" element={<NewsAndUpdate />} />
-          <Route path="guidelines-and-safety-tips" element={<Guidelines />} />
+          <Route path="guidelines-and-safety-tips" element={<GuidelinesAndSafety />} />
           <Route path="user-profile" element={<UserProfile />} />
           <Route path="faq" element={<FAQ />} />
           <Route path="feedback-and-suggestion" element={<FeedbackAndSuggestion />} />
           <Route path="about-us" element={<AboutUs />} />
-          {/* <Route path="footer" element={<Footer />} /> */}
           <Route path="team-and-support" element={<TeamAndSupport />} />
         </Route>
       </Routes>
