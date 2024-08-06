d67a2a3553c027e004e155ccb064f27e2a530542
SanjanaMankar <sanjanamankar22@gnmail.com>
2024-08-06 16:40:18 +0530

    suggestion and feeback form updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import MainLayout from './components/MainLayout';
 import ReportIssue from './pages/ReportIssue.jsx';
 import IssueTracker from './pages/IssueTracker.jsx';
@@ -13,20 +12,6 @@ import TeamAndSupport from './pages/TeamAndSupport.jsx';
 import FAQ from './pages/FAQ.jsx';
 import GuidelinesAndSafety from './pages/GuidelinesAndSafety.jsx';
 import Home from './pages/Home.jsx';
-=======
-import MainLayout from './components/MainLayout.jsx';
-import Home from './Pages/Home.jsx';
-import ReportIssue from './Pages/ReportIssue.jsx';
-import IssueTracker from './Pages/IssueTracker.jsx';
-import GovernmentAction from './Pages/GovernmentAction.jsx';
-import NewsAndUpdate from './Pages/NewsAndUpdate.jsx';
-import UserProfile from './Pages/UserProfile.jsx';
-import FeedbackAndSuggestion from './Pages/FeedbackAndSuggestion.jsx';
-import AboutUs from './Pages/AboutUs.jsx';
-import TeamAndSupport from './Pages/TeamAndSupport.jsx';
-import FAQ from './Pages/FAQ.jsx';
-import GuidelinesAndSafety from './Pages/GuidelinesAndSafety.jsx';
->>>>>>> c8ac631d194d387781de803020adea9d66a7fcbb
 
 function App() {
   return (
@@ -43,6 +28,7 @@ function App() {
           <Route path="user-profile" element={<UserProfile />} />
           <Route path="faq" element={<FAQ />} />
           <Route path="feedback-and-suggestion" element={<FeedbackAndSuggestion />} />
+
           <Route path="about-us" element={<AboutUs />} />
           <Route path="team-and-support" element={<TeamAndSupport />} />
         </Route>
