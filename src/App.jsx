a71eb071126ce9d2434fa30fbee46ee093b6e7fc
Samiksha2498 <samikshamogre2498@gmail.com>
2024-08-07 15:51:21 +0530

    logon signup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/MainLayout';
-import Home from './Pages/Home.jsx';
 import ReportIssue from './Pages/ReportIssue.jsx';
 import IssueTracker from './Pages/IssueTracker.jsx';
 import GovernmentAction from './Pages/GovernmentAction.jsx';
@@ -13,16 +11,6 @@ import TeamAndSupport from './Pages/TeamAndSupport.jsx';
 import FAQ from './Pages/FAQ.jsx';
 import GuidelinesAndSafety from './Pages/GuidelinesAndSafety.jsx';
 import Home from './pages/Home.jsx';
-import ReportIssue from './pages/ReportIssue.jsx';
-import IssueTracker from './pages/IssueTracker.jsx';
-import GovernmentAction from './pages/GovernmentAction.jsx';
-import NewsAndUpdate from './pages/NewsAndUpdate.jsx';
-import UserProfile from './pages/UserProfile.jsx'
-import FeedbackAndSuggestion from './pages/FeedbackAndSuggestion.jsx';
-import AboutUs from './pages/AboutUs.jsx';
-import TeamAndSupport from './pages/TeamAndSupport.jsx';
-import FAQ from './pages/FAQ.jsx';
-import GuidelinesAndSafety from './pages/GuidelinesAndSafety.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 
