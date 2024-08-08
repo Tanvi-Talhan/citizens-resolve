190a94013e2ce921e60819678796999c25c04eaa
Radshu21 <tanvitalhan@gmail.com>
2024-08-08 12:23:45 +0530

    "Issue Tracker and About us Updation"

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Cards = ({ name, title, image }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-auto mt-10">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-[#e0e1dd] mx-auto mt-10">
       <div className="flex justify-center mt-4">
         <img
           className="w-32 h-32 rounded-full object-cover"
@@ -14,7 +14,7 @@ const Cards = ({ name, title, image }) => {
       <div className="px-6 py-4 text-center">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base mb-4">{title}</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-[#003049] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Connect
         </button>
       </div>
